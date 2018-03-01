d002a0871a869b2e36a0842725aeb76a1e9126b7
GZMKII <jiujigong@hotmail.com>
2018-03-02 01:58:03 +0800

    Update Projects

--- data.js
+++ data.js
@@ -1,43 +1,45 @@
-var data = [{
-  "title": "Top",
-  "tag": "Web Design",
-  "bgUrl": "./src/img/top.jpg",
-  "content": '<h1>TochPal Operation Platform</h1><p class="tag">#UI Design   #Information Architecture   #Interation Design   #Visual Design   #Data Visualization   #Back End</p><div class="section">    <div class="two">    <h2>项目总结</h2>    <p>我设计并且迭代了触宝运营平台（简称TOP）。包括设计师端：皮肤编辑、标签图上传、数据统计；运营端：皮肤审核，产品上架、生成报表。我发现三个需要注意的方面：1. 效率2. 专注3. 引导这些方面通过多次用户访谈和反馈得出。然后依据设计原则，针对特定的场景，通过交互设计和视觉设计解决上述问题。</p></div><div class="two">    <h2>Project Summary</h2><p class="detail-p">    I designed and iteration the TOP ( short for TouchPal Operation Platform) . Include designer end: theme edit, data review, sticker edit; OPM end: theme auditing, production stock, report generation. As it turns out, I found 3 major points we shoud notice:    1. Effiency2. Focus3. GuideI draw these conclusion from times of User interview and feedback. After that, I focus on aesthetics, every piece of detial.</p></div></div><img src="./src/img/top-detail-1.jpg">'
-},
-    { "title":"Boom Text",
-      "tag":"UI Design",
-      "bgUrl":"./src/img/boomtext.gif",
-        "content":"<h2>Coming Soon</h2>"
-},
+var data = [
+    {
+        "title":"Hades",
+        "tag":"Web Design",
+        "bgUrl":"./src/img/hades_cover.png",
+        "content":'</div></div><img src="./src/img/hades_detail.jpg">'
+    },
+
   {
-    "title":"BiBi&YoYo Sticker",
+    "title":"Talia AI Assistant",
+    "tag":"UX Design",
+    "bgUrl":"./src/img/talia_cover.gif",
+      "content":"<div></div><img src='./src/img/talia_detail.jpg'>"
+  },
+  {
+    "title":"Bibi&YoyoStickers",
     "tag":"Illustration",
     "bgUrl":"./src/img/bibiyoyo.png",
-      "content":"<h2>Coming Soon</h2>"
+      "content":"<img src='./src/img/sticker_detail.jpg'>"
   },
-  {
-    "title":"NonElectriCity",
-    "tag":"Band Leader&Visual Design",
-    "bgUrl":"./src/img/NonElectriCity.jpg",
-      "content":"<h2>Coming Soon</h2>"
-  },
-  {
-    "title":"Hades",
-    "tag":"Web Design",
-    "bgUrl":"./src/img/theme.png",
-      "content":"<h2>Coming Soon</h2>"
-  },
-  {
-    "title":"Keyboard Themes",
-    "tag":"Visual Design",
-    "bgUrl":"./src/img/theme.gif",
-      "content":"<h2>Coming Soon</h2>"
-  },
+    {
+        "title":"NonElectriCity",
+        "tag":"Band Leader & Visual Design",
+        "bgUrl":"./src/img/NonElectriCity.jpg",
+        "content":"<img src='./src/img/nonelectricity_detail_1.gif'><img src='./src/img/nonelectricity_detail.jpg'>"
+    },
+    {
+        "title": "Top",
+        "tag": "Web Design",
+        "bgUrl": "./src/img/top.jpg",
+        "content": '<img src="./src/img/top-detail-1.jpg">'
+    },
+    { "title":"Boom Text",
+        "tag":"UX Design",
+        "bgUrl":"./src/img/boomtext.gif",
+        "content":"<h2>Coming Soon</h2>"
+    },
     {
         "title":"TouchPal Store V4",
         "tag":"UI Design",
-        "bgUrl":"./src/img/theme.png",
-        "content":"<h2>Coming Soon</h2>"
+        "bgUrl":"./src/img/v4_cover.jpg",
+        "content":"<img src='./src/img/v4_detail.jpg'>"
     }];
 
 var resource = [
